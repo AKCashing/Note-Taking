5b335170df6eff655b3408f9e3c263116a8116e0
cpayment <cpayment12@gmail.com>
2024-01-25 16:38:50 -0600

    Added saveNotes functionality and the ability to revisit previous notes to edit them

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
-const db = require('./db/db.json');
+const fs = require('fs/promises');
 
 const PORT = 3001;
-
 const app = express();
 
 app.use(express.static('public'));
+// Parse JSON requests
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
@@ -16,7 +17,35 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-// app.get('/api/notes', (req, res) => {res.json(db)});
+app.get('/api/notes', async (req, res) => {
+    try {
+        const data = await fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8');
+        const notes = JSON.parse(data);
+        res.json(notes);
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.post('/api/notes', async (req, res) => {
+    try {
+        const newNote = req.body;
+        const data = await fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8');
+        const notes = JSON.parse(data);
+
+        newNote.id = Date.now();
+
+        notes.push(newNote);
+
+        await fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(notes));
+
+        res.json(newNote);
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
